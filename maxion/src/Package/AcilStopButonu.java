d02f5e1862a9ed708652e983c226729b982bbca8
egese <egesevinc@windowslive.com>
2021-10-25 16:41:23 +0300

    try

--- maxion/src/Package/AcilStopButonu.java
+++ maxion/src/Package/AcilStopButonu.java
@@ -1,4 +1,7 @@
 package Package;
 
 public class AcilStopButonu  extends GuvenlikDonanimi{
+    public AcilStopButonu(String name, Boolean isWorking, int licenseNo) {
+        super(name, isWorking, licenseNo);
+    }
 }
